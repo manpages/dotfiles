c6295f644e908742261a9e27c8114c941d7edbb3
Jonn Mostovoy <amarr.industrial@gmail.com>
2013-06-23 13:15:28 +0300

    rc.lua now uses dark solarized theme

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -38,7 +38,7 @@ end
 
 -- {{{ Variable definitions 
 -- Themes define colours, icons, and wallpapers 
-beautiful.init("/usr/share/awesome/themes/zenburn/theme.lua") 
+beautiful.init( awful.util.getdir("config") .. "/themes/awesome-solarized/dark/theme.lua" )
 
 -- This is used later as the default terminal and editor to run.
 terminal = "konsole"
