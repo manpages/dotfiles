4d8368b3c3ae0f117424ba3f705094603e4c6724
Jonn Mostovoy <amarr.industrial@gmail.com>
2013-06-24 22:08:35 +0300

    Update awesome rc.lua

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -41,7 +41,7 @@ end
 beautiful.init( awful.util.getdir("config") .. "/themes/awesome-solarized/dark/theme.lua" )
 
 -- This is used later as the default terminal and editor to run.
-terminal = "konsole"
+terminal = 'st -f Ubuntu\\ Mono'
 applauncher = "dmenu_run" 
 editor = os.getenv("EDITOR") or "vim" 
 editor_cmd = terminal .. " -e " .. editor 
