e7263a10b769a3a2b85302a2e98ffed395b8febd
Jonn Mostovoy <amarr.industrial@gmail.com>
2015-03-03 04:00:02 +0100

    Dotfiles autosync

--- _nixpkgs->config.nix
+++ _nixpkgs->config.nix
@@ -11,6 +11,18 @@ with import /nixpkgs/pkgs/development/haskell-modules/lib.nix { inherit pkgs; };
       cabal2nix
     ]);
 
+    emacsEnv = pkgs.buildEnv {
+      name  = "emacs-env";
+      paths = [
+        pkgs.emacs
+        pkgs.emacs24Packages.rainbowDelimiters
+        pkgs.emacs24Packages.idris
+        pkgs.emacs24Packages.haskellModeGit
+        pkgs.emacs24Packages.gitModes
+        pkgs.emacs24Packages.coffee
+      ];
+    };
+
     lshEnv = pkgs.buildEnv {
       name  = "lsh-env";
       paths = [
@@ -18,7 +30,6 @@ with import /nixpkgs/pkgs/development/haskell-modules/lib.nix { inherit pkgs; };
         pkgs.nodePackages.coffee-script
         pkgs.nodePackages.uglify-js
         pkgs.nodePackages.browserify
-        pkgs.emacs
       ];
     };
 
