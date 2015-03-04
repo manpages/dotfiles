1f6d8e476f79c71cb943243c9386188f2c5f5011
Jonn Mostovoy <amarr.industrial@gmail.com>
2015-03-04 02:55:37 +0100

    Dotfiles autosync

---
+++ nixos/uefi.nix
@@ -0,0 +1,12 @@
+{ config, pkgs, ... }:
+
+{
+  boot.loader.gummiboot.enable = true;
+  boot.loader.gummiboot.timeout = 2;
+  boot.loader.efi.canTouchEfiVariables = true;
+  boot.kernelModules = [ "tp_smapi" ];
+  boot.extraModulePackages = [ config.boot.kernelPackages.tp_smapi ];
+  boot.initrd.postDeviceCommands = ''
+  echo noop > /sys/block/sda/queue/scheduler
+  '';
+}
