5b7ee5845cbd1d6628098c6949052691fe2b03fa
Jonn Mostovoy <amarr.industrial@gmail.com>
2015-08-07 04:33:55 +0200

    Add deployment script

---
+++ deploy.sh
@@ -0,0 +1,26 @@
+[ -z "$1" ] && dest="/home/sweater/github/dotfiles" || dest=$1
+home='/home/sweater'
+for c in \
+    '.bashrc' \
+    '_tmux.conf' \
+    '.nixpkgs/config.nix' \
+    '.newsbeuter/urls' \
+    '.vimrc' \
+    '.tmux.conf' \
+    '.xsession' \
+    '.bash_profile' \
+    '.emacs' \
+    '.vimperatorrc' \
+    '.vimperator/base16-mixedchalk.vimp' \
+    '.vimperator/base16-mixedchalk.uicolors.vimp'
+do
+      cc=${c/\./_}
+      target=${cc//\//->}
+      if [ -f $home/$c ]; then
+	  cp -v $home/$c{,.backup}
+      fi
+      cp -v $dest/$target $home/$c
+done
+mkdir -p $home/.emacs.d
+cp -vru _emacs.d/wilderness $home/.emacs.d/
+cp -ru _i3/ $home/.i3
