4eaa2efd516a764c4598ab867a36fda5850263a4
Jonn Mostovoy <amarr.industrial@gmail.com>
2015-11-03 18:55:14 +0100

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -12,15 +12,16 @@ for c in \
     '.emacs' \
     '.vimperatorrc' \
     '.vimperator/base16-mixedchalk.vimp' \
-    '.vimperator/base16-mixedchalk.uicolors.vimp'
+    '.vimperator/base16-mixedchalk.uicolors.vimp' \
+    '.ghcPkgUtils.source' \
+    '.history.source'
 do
       cc=${c/\./_}
       target=${cc//\//->}
       if [ -f $home/$c ]; then
-	  cp -v $home/$c{,.backup}
+        cp -v $home/$c{,.backup}
       fi
       cp -v $dest/$target $home/$c
 done
 mkdir -p $home/.emacs.d
 cp -vru _emacs.d/wilderness $home/.emacs.d/
-cp -ru _i3/ $home/.i3
